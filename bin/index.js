a464147bb02fec16ab413558454d6ce67fe5090f
shb190802 <609690891@qq.com>
2019-11-08 10:36:49 +0800

    修改

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
+
 const commander = require('commander')
 const program = new commander.Command()
 const Project = require('../src/project')
+const package = require('../package.json')
 
 program
+  .version(package.version)
   .option('-n, --name[name]','项目名称')
   .option('-d, --desc[desc]','项目介绍')
   .parse(process.argv)
--- src/utils.js
+++
@@ -1,9 +0,0 @@
-const fse = require('fs-extra')
-
-function getDirFiles(dir) {
-  try {
-    let files = fse.readFileSync(dir)
-  }catch(e){
-    return []
-  }
-}
